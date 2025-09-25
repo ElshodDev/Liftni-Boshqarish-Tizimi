7126208c9309ccfa7b2e584a96e212cca76a9528
Elshod <elshodibadullayev28@gmail.com>
2025-09-25 07:18:57 +0100

    INFRA:Initialize API Project

---
+++ Liftni-Boshqarish-Tizimi.Api/Startup.cs
@@ -0,0 +1,59 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: Liftni_Boshqarish_Tizimi.Api
+//===================================================
+
+using Microsoft.OpenApi.Models;
+
+namespace Liftni_Boshqarish_Tizimi.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiInfo = new OpenApiInfo
+            {
+                Title = "ElevatorService.Api",
+                Version = "v1"
+            };
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: apiInfo);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "ElevatorService.Api v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+    }
+}
