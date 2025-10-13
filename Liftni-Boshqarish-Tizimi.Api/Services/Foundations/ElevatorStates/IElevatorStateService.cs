58935f00f2e2256344e8aa71b1dc54fb33bbdfa2
Elshod <elshodibadullayev28@gmail.com>
2025-10-14 01:13:38 +0500

    ShouldAddElevatorStateAsync()=>FAIL

---
+++ Liftni-Boshqarish-Tizimi.Api/Services/Foundations/ElevatorStates/IElevatorStateService.cs
@@ -0,0 +1,15 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: Liftni_Boshqarish_Tizimi.Api
+//===================================================
+
+using Liftni_Boshqarish_Tizimi.Api.Models.Foundations.ElevatorStates;
+
+namespace Liftni_Boshqarish_Tizimi.Api.Services.Foundations.ElevatorStates
+{
+    public interface IElevatorStateService
+    {
+        ValueTask<ElevatorState> AddElevatorStateAsync(ElevatorState elevatorState);
+    }
+}
