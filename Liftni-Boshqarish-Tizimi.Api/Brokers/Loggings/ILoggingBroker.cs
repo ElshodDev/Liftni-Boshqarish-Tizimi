d0b4162bc48d4e146852a5b234d8d49c4e5f6355
Elshod <elshodibadullayev28@gmail.com>
2025-10-18 17:24:43 +0500

    BROKERS: Implement  Logging

---
+++ Liftni-Boshqarish-Tizimi.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: Liftni_Boshqarish_Tizimi.Api
+//===================================================
+
+namespace Liftni_Boshqarish_Tizimi.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception); 
+        void LogCritical(Exception exception);
+    }
+}
---
+++ Liftni-Boshqarish-Tizimi.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,21 @@
+﻿//===================================================
+// Copyright (c) 2025 Elshod Ibadullayev
+// Free To Use For Learning and Development
+// Project: Liftni_Boshqarish_Tizimi.Api
+//===================================================
+
+namespace Liftni_Boshqarish_Tizimi.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+    }
+}
